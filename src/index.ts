0b6cd81acfa31454ecc0ad0c0bdd19785cf69979
rohit-s-init <rohitrameshwarsawant@gmail.com>
2026-03-19 19:32:20 +0530

    adding url encoded middleware

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,8 @@ import cors from "cors"
 
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); // for JSON APIs
+app.use(express.urlencoded({ extended: true })); // 🔥 for Twilio
 app.use(cors())
 
 app.use("/api", router)
